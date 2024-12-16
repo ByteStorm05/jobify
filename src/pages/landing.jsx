d2c0f68825efb5c29ba837df487d99a50f132201
ByteStorm05 <143643985+ByteStorm05@users.noreply.github.com>
2024-12-16 14:19:44 +0530

    Carousel Done

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { Button } from '../components/ui/button';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import companies from "../data/companies.json";
 
 const LandingPage = () => {
   return (
-    <main>
-      <section className="text-center py-20">
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+      <section className="text-center pt-0">
         <h1 className="flex flex-col items-center justify-center font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tight py-4">
           Your Dream Job Awaits
           <span className="flex items-center justify-center gap-2 sm:gap-4 mt-4">
@@ -37,9 +44,31 @@ const LandingPage = () => {
         </Link>
       </div>
 
-
       {/* Carousel */}
+
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 2000,
+          }),
+        ]}
+        className="w-full py-10"
+      >
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => (
+            <CarouselItem key={id} className="basis-1/3 lg:basis-1/6 ">
+              <img
+                src={path}
+                alt={name}
+                className="h-9 sm:h-14 object-contain"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
   
+
+
       {/* banner */}
   
       <section>
